f9bfb14682e2d3e6176332af811e0c3f495ea3a3
DSS T Level for TMC <1458431@stu.tmc.ac.uk>
2023-01-12 17:26:41 +0000

    Update data.js

--- global/data.js
+++ global/data.js
@@ -10,7 +10,7 @@ document.getElementById("footer").innerHTML =
     +'        <li class="list-inline-item"><a href="https://dsstlevel.github.io/README.md">About</a></li>'
     +'        <li class="list-inline-item"><a href="#">Top of Page</a></li>'
     +'    </ul>'
-    +'    <p class="copyright">Digital Support Services TMC</p>'
+    +'    <p class="copyright">Digital Support Services @ TMC</p>'
     +'</footer>'
     +'</div>'
     +'<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>'
