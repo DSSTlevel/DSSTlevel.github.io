a0e8092aa7ade15bf1c5ccd6cb98eaef50bc820f
DSS T Level for TMC <1458431@stu.tmc.ac.uk>
2023-01-13 00:00:26 +0000

    a

--- global/data.js
+++ global/data.js
@@ -13,40 +13,42 @@ document.getElementById("footer").innerHTML =
   </footer>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>`
+
 document.getElementById("asidebar").innerHTML =
-    '<div class="sidebar">'
-    +'<button class="dropdown-btn">Business Context</button>'
-    +'<div class="dropdown-container">'
-    +'</div>'
-    +'<button class="dropdown-btn">Culture</button>'
-    +'<div class="dropdown-container">'
-    +'</div>'
-    +'<button class="dropdown-btn">Data</button>'
-    +'<div class="dropdown-container">'
-    +'</div>'
-    +'<button class="dropdown-btn">Digital Analysis</button>'
-    +'<div class="dropdown-container">'
-    +'</div>'
-    +'<button class="dropdown-btn">Digital Environments</button><div class="dropdown-container"><a href="Web/Computers/Components.html">Components</a><a href="Web/Networking/protocols.html">Protocols</a><a href="Web/Networking/NetworkingComponents.html">NetComponents</a><a href="Web/Computers/OS.html">OS</a></div>'
-    +'<button class="dropdown-btn">Diversity and Inclusion</button>'
-    +'<div class="dropdown-container">'
-    +'</div>'
-    +'<button class="dropdown-btn">Learning</button>'
-    +'<div class="dropdown-container">'
-    +'</div>'
-    +'<button class="dropdown-btn">Legislation</button>'
-    +'<div class="dropdown-container">'
-    +'   <a href="Web/Ethics/Laws-Regs.html">Laws & Regs</a>'
-    +'</div>'
-    +'<button class="dropdown-btn">Planning</button>'
-    +'<div class="dropdown-container">'
-    +'</div>'
-    + '<button class="dropdown-btn">Security</button>'
-    +'<div class="dropdown-container">'
-    +'   <a href="Web/Computers/Threats.html">Threats</a>'
-    +'</div>'
-    +'<button class="dropdown-btn">Tools</button>'
-    +'<div class="dropdown-container">'
-    +'</div>'
-    +'<button class="dropdown-btn">Sources</button><div class="dropdown-container"><a href="https://www.netacad.com/portal/learning" target="_blank">Cisco</a><a href="https://ncfe.org.uk" target="_blank">NCFE Website</a><a href="https://www.qualhub.co.uk/qualification-search/qualification-detail/t-level-technical-qualification-in-digital-support-services-level-3-delivered-by-5034?login=false&originalUrl=https%3A%2F%2Fwww.qualhub.co.uk.mcas.ms%2Fqualification-search%2Fqualification-detail%2Ft-level-technical-qualification-in-digital-support-services-level-3-delivered-by-5034%3FMcasTsid%3D20892&McasCSRF=2a04a17a9a924bcc526b4bd772189cf149d6804c98bb8c2c08d559e394f185f9" target="_blank">DSS Past Papers</a></div>'
-    +'</div>';
+`<div class="sidebar">
+  <button class="dropdown-btn">Business Context</button>
+  <div class="dropdown-container"></div>
+  <button class="dropdown-btn">Culture</button>
+  <div class="dropdown-container"></div>
+  <button class="dropdown-btn">Data</button>
+  <div class="dropdown-container"></div>
+  <button class="dropdown-btn">Digital Analysis</button>
+  <div class="dropdown-container"></div>
+  <button class="dropdown-btn">Digital Environments</button>
+  <div class="dropdown-container">
+    <a href="Web/Soft-Hardware.html">Software/Hardware</a>
+    <a href="Web/Networking.html">Networking</a>
+  </div>
+  <button class="dropdown-btn">Diversity and Inclusion</button>
+  <div class="dropdown-container"></div>
+  <button class="dropdown-btn">Learning</button>
+  <div class="dropdown-container"></div>
+  <button class="dropdown-btn">Legislation</button>
+  <div class="dropdown-container">
+    <a href="Web/Ethics/Laws-Regs.html">Laws & Regs</a>
+  </div>
+  <button class="dropdown-btn">Planning</button>
+  <div class="dropdown-container"></div>
+  <button class="dropdown-btn">Security</button>
+  <div class="dropdown-container">
+    <a href="Web/Computers/Threats.html">Threats</a>
+  </div>
+  <button class="dropdown-btn">Tools</button>
+  <div class="dropdown-container"></div>
+  <button class="dropdown-btn">Sources</button>
+  <div class="dropdown-container">
+    <a href="https://www.netacad.com/portal/learning" target="_blank">Cisco</a>
+    <a href="https://ncfe.org.uk" target="_blank">NCFE Website</a>
+    <a href="https://www.qualhub.co.uk/qualification-search/qualification-detail/t-level-technical-qualification-in-digital-support-services-level-3-delivered-by-5034?login=false&originalUrl=https%3A%2F%2Fwww.qualhub.co.uk.mcas.ms%2Fqualification-search%2Fqualification-detail%2Ft-level-technical-qualification-in-digital-support-services-level-3-delivered-by-5034%3FMcasTsid%3D20892&McasCSRF=2a04a17a9a924bcc526b4bd772189cf149d6804c98bb8c2c08d559e394f185f9" target="_blank">DSS Past Papers</a>
+  </div>
+</div>`
