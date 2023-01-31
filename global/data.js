dd0a32d3b4784947d3f1b6084bc2f135809009ab
DSS T Level for TMC <1458431@stu.tmc.ac.uk>
2023-01-31 13:51:02 +0000

    a

--- global/data.js
+++ global/data.js
@@ -95,6 +95,7 @@ document.getElementById("navigbar").innerHTML =
   <ul class="dropdown-content">
     <li class="dropdown-sub">
       <a href="https://www.netacad.com/portal/learning" target="_blank">Cisco</a>
+      <a href="https://dsstlevel.github.io/Web/html/codingScripts" target="_blank">Cisco</a>
       <li class="dropdown-sub">
       <a>NCFE</a>
       <ul class="dropdown-sub-list">
