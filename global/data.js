85f0225e4537351a9ee6b6d5625b9dbab13b79b5
DSS T Level for TMC <1458431@stu.tmc.ac.uk>
2023-02-01 12:07:30 +0000

    a

--- global/data.js
+++ global/data.js
@@ -25,7 +25,7 @@ document.getElementById("navigbar").innerHTML =
   <ul id="nav_ul">
   <a href="https://dsstlevel.github.io/index.html"><i class="fa fa-home" style="font-size:20px;color:white"></i></a>
   <li class="dropdown">
-  <a><i class="fa-solid fa-bars"></i>Paper A</a>
+  <a><i class="fa-solid fa-bars"></i><i class="fa-regular fa-file-lines"> </i><i class="fa-solid fa-a"></i></a>
   <ul class="dropdown-content">
     <li class="dropdown-sub">
       <a>Business Context</a>
@@ -61,7 +61,7 @@ document.getElementById("navigbar").innerHTML =
   </ul>
   </li>
   <li class="dropdown">
-  <a><i class="fa-solid fa-bars"></i>Paper B</a>
+  <a><i class="fa-solid fa-bars"></i><i class="fa-regular fa-file-lines"></i> <i class="fa-solid fa-b"></i></a>
   <ul class="dropdown-content">
     <li class="dropdown-sub">
       <a>Testing</a>
@@ -94,7 +94,7 @@ document.getElementById("navigbar").innerHTML =
   </ul>
   </li>
   <li class="dropdown">
-  <a><i class="fa-solid fa-bars"></i>Resources</a>
+  <a><i class="fa-solid fa-bars"></i> <i class="fa-solid fa-newspaper"></i></a>
   <ul class="dropdown-content">
     <li class="dropdown-sub">
       <a href="https://www.netacad.com/portal/learning" target="_blank">Cisco</a>
