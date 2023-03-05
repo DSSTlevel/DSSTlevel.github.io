b74394851187dcd2738521cee217245807e63c76
gmgwhDM2005 <jayndon121@outlook.com>
2023-03-05 13:52:09 +0000

    a

--- global/settings.js
+++ global/settings.js
@@ -8,5 +8,5 @@ var timetable={
     wednesday: `L1. 9:00-10:45<br>B. 10:45-11:00<br>L2. 11:00-12:45<br>B. 12:45-13:15<br>L3. 13:15-15:00`,
     thursday: `Online. 9:00-12:00`,
     friday: `L1. 9:00-10:00<br>B. 10:00-11:15<br>L2. 11:15-12:15<br>B. 12:15-13:00<br>L3. 13:00-15:00`,
-    weekend: `It's the weekend! 😀`
+    weekend: `It's the weekend!`
 }
