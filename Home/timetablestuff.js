0ecf8f9b62a57a9cfbb895453346676ec69cfa32
DSS T Level for TMC <107148267+gmgwhDM2005@users.noreply.github.com>
2023-08-27 00:43:47 +0100

    p

---
+++ Home/timetablestuff.js
@@ -0,0 +1,162 @@
+const CurrentDate = new Date().getDay()
+
+/*
+1: Monday
+2: Tuesday
+3: Wednesday
+4: Thursday
+5: Friday
+*/
+
+switch (CurrentDate) {
+    case 6:
+        document.getElementById("timetable").innerHTML = `
+        <div class="lesson">
+            <div class="subject">Lesson 1</div>
+            <div class="teacher">T. D</div>
+            <div class="time">9:00AM - 10:30AM (1.5)</div>
+            <div class="room">CCM210</div>
+        </div>
+        <div class="break">
+            <div class="name">Break</div>
+            <div class="duration">15 Minutes</div>
+        </div>
+        <div class="lesson">
+            <div class="subject">Lesson 2</div>
+            <div class="teacher">T. D</div>
+            <div class="time">10:45AM - 12:15AM (1.5)</div>
+            <div class="room">CCM210</div>
+        </div>
+        <div class="break">
+            <div class="name">Lunch</div>
+            <div class="duration">45 Minutes</div>
+        </div>
+        <div class="lesson">
+            <div class="subject">Lesson 3</div>
+            <div class="teacher">A. M</div>
+            <div class="time">13:00PM - 14:45PM (1.75)</div>
+            <div class="room">CCM210</div>
+        </div>
+        <div class="break">
+            <div class="name">Break</div>
+            <div class="duration">15 Minutes</div>
+        </div>
+        <div class="lesson">
+            <div class="subject">Lesson 4</div>
+            <div class="teacher">A. M</div>
+            <div class="time">15:00PM - 16:45PM (1.75)</div>
+            <div class="room">CCM210</div>
+        </div>
+        `
+        break;
+    case 2:
+        document.getElementById("timetable").innerHTML = `
+        <div class="lesson">
+            <div class="subject">Tutorial</div>
+            <div class="teacher">A. W</div>
+            <div class="time">9:30AM - 11:00AM (1.5)</div>
+            <div class="room">TBC</div>
+        </div>
+        <div class="break">
+            <div class="name">Break</div>
+            <div class="duration">15 Minutes</div>
+        </div>
+        <div class="lesson">
+            <div class="subject">Employability</div>
+            <div class="teacher">C. G</div>
+            <div class="time">11:15AM - 12:15AM (1)</div>
+            <div class="room">TBC</div>
+        </div>
+        <div class="break">
+            <div class="name">Lunch</div>
+            <div class="duration">45 Minutes</div>
+        </div>
+        <div class="lesson">
+            <div class="subject">Lesson 3</div>
+            <div class="teacher">A. M</div>
+            <div class="time">13:00PM - 14:45PM (1.75)</div>
+            <div class="room">CCM205</div>
+        </div>
+        <div class="break">
+            <div class="name">Break</div>
+            <div class="duration">15 Minutes</div>
+        </div>
+        <div class="lesson">
+            <div class="subject">Lesson 4</div>
+            <div class="teacher">A. M</div>
+            <div class="time">15:00PM - 16:45PM (1.75)</div>
+            <div class="room">CCM205</div>
+        </div>
+        `
+        break;
+    case 3:
+        document.getElementById("timetable").innerHTML = `
+        <div class="lesson">
+            <div class="subject">Lesson 1</div>
+            <div class="teacher">A. M</div>
+            <div class="time">9:00AM - 10:30AM (1.5)</div>
+            <div class="room">CCM210</div>
+        </div>
+        <div class="break">
+            <div class="name">Break</div>
+            <div class="duration">15 Minutes</div>
+        </div>
+        <div class="lesson">
+            <div class="subject">Lesson 2</div>
+            <div class="teacher">A. M</div>
+            <div class="time">10:45AM - 12:15AM (1.5)</div>
+            <div class="room">CCM210</div>
+        </div>
+        <div class="break">
+            <div class="name">Lunch</div>
+            <div class="duration">45 Minutes</div>
+        </div>
+        <div class="lesson">
+            <div class="subject">Lesson 3</div>
+            <div class="teacher">A. M</div>
+            <div class="time">13:00PM - 15:00PM (2)</div>
+            <div class="room">CCM204</div>
+        </div>
+        `
+        break;
+    case 4:
+        document.getElementById("timetable").innerHTML = `
+        <div class="lesson">
+            <div class="time"></div>
+            <div class="subject">Take the day off</div>
+            <div class="teacher">You deserve it!</div>
+            <div class="room"></div>
+        </div>
+        `
+        break;
+    case 5:
+        document.getElementById("timetable").innerHTML = `
+        <div class="lesson">
+            <div class="subject">Lesson 1</div>
+            <div class="teacher">D. T</div>
+            <div class="time">9:00AM - 10:30AM (1.5)</div>
+            <div class="room">CCM210</div>
+        </div>
+        <div class="break">
+            <div class="name">Break</div>
+            <div class="duration">15 Minutes</div>
+        </div>
+        <div class="lesson">
+            <div class="subject">Lesson 2</div>
+            <div class="teacher">A. M</div>
+            <div class="time">10:45AM - 12:15AM (1.5)</div>
+            <div class="room">CCM210</div>
+        </div>
+        `
+        break;
+    default:
+        document.getElementById("timetable").innerHTML = `
+        <div class="lesson">
+            <div class="time"></div>
+            <div class="subject">The Weekend</div>
+            <div class="teacher">Saturday/Sunday</div>
+            <div class="room"></div>
+        </div>
+        `
+        break;
+}
